ee4fa7f12da4a70b7a75581e12fc3f7982c79a35
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-06-20 10:16:56 +0000

    Deploy jplomas/LapRiskCalc to github.com/jplomas/LapRiskCalc.git:gh-pages

---
+++ js/78.2631cdc0.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkporc"]=self["webpackChunkporc"]||[]).push([[78],{6078:(e,t,n)=>{n.r(t),n.d(t,{startStatusTap:()=>a});var r=n(65),s=n(8487),o=n(6587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,r.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const c=(0,s.a)(a);c&&new Promise((e=>(0,o.c)(c,e))).then((()=>{(0,r.Iu)((async()=>{c.style.setProperty("--overflow","hidden"),await(0,s.s)(c,300),c.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78.2631cdc0.js.map
---
+++ js/990.1470ac27.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkporc"]=self["webpackChunkporc"]||[]).push([[990],{8990:(t,e,n)=>{n.r(e),n.d(e,{createSwipeBackGesture:()=>s});var r=n(6587),o=n(545),c=n(6515);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const s=(t,e,n,s,a)=>{const i=t.ownerDocument.defaultView,u=(0,o.i)(t),h=t=>{const e=50,{startX:n}=t;return u?n>=i.innerWidth-e:n<=e},l=t=>u?-t.deltaX:t.deltaX,d=t=>u?-t.velocityX:t.velocityX,p=t=>h(t)&&e(),k=t=>{const e=l(t),n=e/i.innerWidth;s(n)},w=t=>{const e=l(t),n=i.innerWidth,o=e/n,c=d(t),s=n/2,u=c>=0&&(c>.2||e>s),h=u?1-o:o,p=h*n;let k=0;if(p>5){const t=p/Math.abs(c);k=Math.min(t,540)}a(u,o<=0?.01:(0,r.j)(0,o,.9999),k)};return(0,c.A)({el:t,gestureName:"goback-swipe",gesturePriority:40,threshold:10,canStart:p,onStart:n,onMove:k,onEnd:w})}}}]);
+//# sourceMappingURL=990.1470ac27.js.map
