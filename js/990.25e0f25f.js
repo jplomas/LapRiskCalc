65704b68d55984cd5de733429aaeac33a7dcb4ef
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2023-04-20 08:42:12 +0000

    Deploy jplomas/LapRiskCalc to github.com/jplomas/LapRiskCalc.git:gh-pages

--- js/990.25e0f25f.js
+++ js/990.25e0f25f.js
@@ -3,4 +3,4 @@
  * (C) Ionic http://ionicframework.com - MIT License
  */
 const s=(t,e,n,s,a)=>{const i=t.ownerDocument.defaultView,u=(0,o.i)(t),h=t=>{const e=50,{startX:n}=t;return u?n>=i.innerWidth-e:n<=e},l=t=>u?-t.deltaX:t.deltaX,d=t=>u?-t.velocityX:t.velocityX,p=t=>h(t)&&e(),k=t=>{const e=l(t),n=e/i.innerWidth;s(n)},w=t=>{const e=l(t),n=i.innerWidth,o=e/n,c=d(t),s=n/2,u=c>=0&&(c>.2||e>s),h=u?1-o:o,p=h*n;let k=0;if(p>5){const t=p/Math.abs(c);k=Math.min(t,540)}a(u,o<=0?.01:(0,r.j)(0,o,.9999),k)};return(0,c.A)({el:t,gestureName:"goback-swipe",gesturePriority:40,threshold:10,canStart:p,onStart:n,onMove:k,onEnd:w})}}}]);
-//# sourceMappingURL=990.1470ac27.js.map
+//# sourceMappingURL=990.25e0f25f.js.map
