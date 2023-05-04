95930aae59f1dc5362bcd5219d90776ac3291455
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2023-05-04 13:47:18 +0000

    Deploy jplomas/LapRiskCalc to github.com/jplomas/LapRiskCalc.git:gh-pages

--- js/78.9b2b4c72.js
+++ js/78.9b2b4c72.js
@@ -3,4 +3,4 @@
  * (C) Ionic http://ionicframework.com - MIT License
  */
 const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,r.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const c=(0,s.a)(a);c&&new Promise((e=>(0,o.c)(c,e))).then((()=>{(0,r.Iu)((async()=>{c.style.setProperty("--overflow","hidden"),await(0,s.s)(c,300),c.style.removeProperty("--overflow")}))}))}))}))}}}]);
-//# sourceMappingURL=78.240d9a2a.js.map
+//# sourceMappingURL=78.9b2b4c72.js.map
