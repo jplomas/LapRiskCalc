0ed34bf6291c22e8cb750fc440ea97d7197d6e78
JP Lomas <jp.lomas@gmail.com>
2025-04-10 16:02:52 +0100

    Edit Cypress config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  integrationFolder: 'tests/e2e/specs',
-  supportFile: 'tests/e2e/support/index.js',
-  testFiles: '**/*.{js,jsx,ts,tsx}',
-  video: false,
-  screenshotOnRunFailure: false,
-  baseUrl: 'http://localhost:8080',
+  e2e: {
+    integrationFolder: 'tests/e2e/specs',
+    supportFile: 'tests/e2e/support/index.js',
+    testFiles: '**/*.{js,jsx,ts,tsx}',
+    video: false,
+    screenshotOnRunFailure: false,
+    baseUrl: 'http://localhost:8080',
+  },
   component: {
     testFiles: 'tests/unit/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     supportFile: false,
